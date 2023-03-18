3b4d770f17f44522467ea17f457e6adb02a7bbd8
Fisnik Kurti <47564531+fisnikkk@users.noreply.github.com>
2023-03-18 15:35:57 +0100

    new changes

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -3,10 +3,20 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import MyModel
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 class MyModelListView(ListView):
     model = MyModel
     template_name = 'my_model_list.html'
+    paginate_by = 10  # Show 10 items per page
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.GET.get("search", "")
+        if search_term:
+            queryset = queryset.filter(Q(name__icontains=search_term) | Q(description__icontains=search_term))
+        return queryset
 
 class MyModelDetailView(DetailView):
     model = MyModel
