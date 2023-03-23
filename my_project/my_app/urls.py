b97d47dc3c5e75dc26ad406c611d039d1babd7d7
Fisnik Kurti <47564531+fisnikkk@users.noreply.github.com>
2023-03-23 16:38:37 +0100

    changes

--- my_project/my_app/urls.py
+++ my_project/my_app/urls.py
@@ -5,6 +5,7 @@ from .models import BlogPost, UserProfile, User
 
 urlpatterns = [
     path('', views.BlogPostListView.as_view(), name='blog_post_list'),
+    
     path('register/', views.register, name='register'),
     path('user_profiles/', views.user_profile_list, name='user_profile_list'),
     path('user_profiles/create/', views.UserProfileCreateView.as_view(), name='user_profile_create'),
