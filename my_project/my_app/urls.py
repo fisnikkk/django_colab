f18c59b10bb681d3b7bb2bcc29b3da3bc7d07d1c
Fisnik Kurti <47564531+fisnikkk@users.noreply.github.com>
2023-03-23 10:50:39 +0100

    changes

--- my_project/my_app/urls.py
+++ my_project/my_app/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .models import BlogPost, UserProfile, User
 
 urlpatterns = [
-    path('', views.my_model_list, name='my_model_list'),
+    path('', views.BlogPostListView.as_view(), name='blog_post_list'),
     path('register/', views.register, name='register'),
     path('user_profiles/', views.user_profile_list, name='user_profile_list'),
     path('user_profiles/create/', views.UserProfileCreateView.as_view(), name='user_profile_create'),
     path('user_profiles/<int:pk>/update/', views.UserProfileUpdateView.as_view(), name='user_profile_update'),
     path('user_profiles/<int:pk>/delete/', views.UserProfileDeleteView.as_view(), name='user_profile_delete'),
-    path('my_model/<int:pk>/', views.MyModelDetailView.as_view(), name='my_model_detail'),
-    path('my_model/create/', views.MyModelCreateView.as_view(), name='my_model_create'),
-    path('my_model/<int:pk>/update/', views.MyModelUpdateView.as_view(), name='my_model_update'),
-    path('my_model/<int:pk>/delete/', views.MyModelDeleteView.as_view(), name='my_model_delete'),
+    path('blog_post/<int:pk>/', views.BlogPostDetailView.as_view(), name='blog_post_detail'),
+    path('blog_post/create/', views.BlogPostCreateView.as_view(), name='blog_post_create'),
+    path('blog_post/<int:pk>/update/', views.BlogPostUpdateView.as_view(), name='blog_post_update'),
+    path('blog_post/<int:pk>/delete/', views.BlogPostDeleteView.as_view(), name='blog_post_delete'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -1,39 +1,44 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
-from .models import MyModel, UserProfile, User
+from django.views.generic.list import ListView
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import UserProfileForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.forms import UserCreationForm
+from .models import BlogPost, UserProfile, User
 
-def my_model_list(request):
-    models = MyModel.objects.all()
-    return render(request, 'my_model_list.html', {'models': models})
+
 
 def user_profile_list(request):
     users = User.objects.all()
     print(f'User profiles queryset (in user_profile_list): {users}')
     return render(request, 'user_profile_list.html', {'users': users})
 
-class MyModelDetailView(DetailView):
-    model = MyModel
-    template_name = 'my_model_detail.html'
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog_post_detail.html'
 
-class MyModelCreateView(CreateView):
-    model = MyModel
-    fields = ['name', 'description']
-    template_name = 'my_model_create.html'
+class BlogPostCreateView(CreateView):
+
+    model = BlogPost
+    fields = ['title', 'author', 'content']
+
+    template_name = 'blog_post_create.html'
     success_url = reverse_lazy('my_model_list')
 
-class MyModelUpdateView(UpdateView):
-    model = MyModel
-    fields = ['name', 'description']
-    template_name = 'my_model_update.html'
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    fields = ['title', 'author', 'content']
+
+    template_name = 'blog_post_update.html'
     success_url = reverse_lazy('my_model_list')
 
-class MyModelDeleteView(DeleteView):
-    model = MyModel
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
     template_name = 'my_model_delete.html'
     success_url = '/'
 
@@ -55,18 +60,19 @@ class UserProfileDeleteView(DeleteView):
     template_name = 'user_profile_confirm_delete.html'
     success_url = '/user_profiles/'
 
+@method_decorator(login_required, name='dispatch')
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = 'blog_post_list.html'
+    context_object_name = 'models'
+
 def register(request):
     if request.method == 'POST':
-        form = UserProfileForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            UserProfile.objects.create(user=user)
             login(request, user)
-            return redirect('my_model_list')
-        else:
-            for field, errors in form.errors.as_data().items():
-                for error in errors:
-                    messages.error(request, f"{field}: {error}")
+            return redirect('blog_post_list.html')
     else:
-        form = UserProfileForm()
+        form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
