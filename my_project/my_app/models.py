ea390b1a167c4f5ca80f25a8ea48345dba7ef56f
Fisnik Kurti <47564531+fisnikkk@users.noreply.github.com>
2023-03-23 10:50:19 +0100

    now its more of a blog

--- my_project/my_app/models.py
+++ my_project/my_app/models.py
@@ -4,12 +4,16 @@ from django.contrib.auth.models import User
 
 
 
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def get_absolute_url(self):
-        return reverse('my_model_detail', args=[str(self.pk)])
+    def __str__(self):
+        return self.title
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
